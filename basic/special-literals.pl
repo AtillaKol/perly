93b8f6fbaf618430d175e9b5b202eb47160fa716
Atilla Kolali <atillakolali22@gmail.com>
2023-06-26 21:08:42 +0200

    some special literals

---
+++ basic/special-literals.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $filename = __FILE__;
+my $linecnt = __LINE__;
+my $package = __PACKAGE__;
+
+print "Filename is $filename\n";
+print "Line count is $linecnt\n";
+print "Package name is $package\n";
