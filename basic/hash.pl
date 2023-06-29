d686e95ae71e3a3116211ffb2497b7c51d1fc828
Atilla Kolali <atillakolali22@gmail.com>
2023-06-29 21:37:15 +0200

    added exmaple for adding and deleting elements in a hash

--- basic/hash.pl
+++ basic/hash.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # Creates a hash
-my %data = (1 => "FCSG", 2 => 'FCZ', 3 => 'FCB', 4 => 'GC', 5 =>'YB');
+my %data = (1 => "FCSG", 2 => "FCZ", 3 => "FCB", 4 => "GC", 5 => "YB");
 
 # Print the value of the key 1.
 print "The best of the best: $data{1}\n";
@@ -18,3 +18,10 @@ print "Ranks: @ranks\n";
 
 # Print size of the hash
 print "Size of the hash is: ",  scalar @all_teams,"\n";
+
+# Add element with key 6
+$data{6} = "FCW";
+print "6th place: $data{6}\n";
+
+# Delete element with key 6
+delete $data{6};
