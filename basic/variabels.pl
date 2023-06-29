f33e1bad32e664d6027714a5491c47355b6b81c6
Atilla Kolali <atillakolali22@gmail.com>
2023-06-29 21:07:57 +0200

    removed hash exmaples

--- basic/variabels.pl
+++ basic/variabels.pl
@@ -8,13 +8,6 @@ my $age=25;
 my $name="Zlatan Ibrahimovic";
 my $salary=655555.45;
 
-# Hash
-my %first_hash = (1, "Hans Peter", 2, "Homer Simpson");
-
 print "Age: $age\n";
 print "Name: $name\n";
 print "Salary: $salary\n";
-
-
-print "1 -> $first_hash{1}\n";
-print "2 -> $first_hash{2}\n";
