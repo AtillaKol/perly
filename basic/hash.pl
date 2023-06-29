963d618322349ea45f3f4063e159a550373efe65
Atilla Kolali <atillakolali22@gmail.com>
2023-06-29 21:13:36 +0200

    initial commit of hash examples

---
+++ basic/hash.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Creates a hash
+my %data = (1 => "FCSG", 2 => 'FCZ', 3 => 'FCB', 4 => 'GC', 5 =>'YB');
+
+# Print the value of the key 1.
+print "The best of the best: $data{1}\n";
